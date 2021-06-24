2f3d540fdd4ecb52ab3d91547c511d5b09351321
kobidsim <simkhadakobid@gmail.com>
2021-06-24 11:16:14 +0545

    Requests go in separate threads so its much faster(2-3 seconds) to finish all requests. TODO:Make a parser that takes raw request and creates a request accordingly without User Interaction with the code

--- myIntruder.py
+++ myIntruder.py
@@ -17,8 +17,23 @@ def sendGetRequest(domain, payload):
 	return r
 
 payloads = getPayload()
-domain = 'https://www.youtube.com/'
-for payload in payloads:
+domain = 'https://ac091fe61f7388628064ac9700900030.web-security-academy.net/'
+
+def sendIt(payload):
 	response = sendGetRequest(domain, payload)
 	if response.status_code == 200:
-		print(payload, response.status_code, response.length)
+		print(payload, response.status_code)
+
+threads = []
+
+for payload in payloads:
+	t = threading.Thread(target=sendIt, args=[payload])
+	threads.append(t)
+
+for t in threads:
+	t.start()
+
+for t in threads:
+	t.join()
+
+print('Success')
